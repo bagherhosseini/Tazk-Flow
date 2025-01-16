1b403d4b86b5b84450ce953838a3357369a51cd7
Bagher hosseini <Bagher.Hosseini@yh.nackademin.se>
2025-01-16 07:09:48 +0100

    feat: api service to send request

---
+++ app/services/api.tsx
@@ -0,0 +1,80 @@
+// Types
+export type Project = {
+    id: string;
+    name: string;
+    description: string;
+    status: 'active' | 'completed' | 'on_hold';
+    teamId?: string;
+    task_statuses: string[];
+    createdAt: string;
+    due_date?: string;
+};
+
+export type Task = {
+    id: string;
+    title: string;
+    description: string;
+    status: string;
+    priority: 'low' | 'medium' | 'high';
+    dueDate: string;
+    createdAt: string;
+    project?: {
+        id: string;
+        name: string;
+    };
+    assignedTo: string;
+    createdBy: string;
+    tags: string[];
+};
+
+export type Comment = {
+    id: string;
+    taskId: string;
+    content: string;
+    createdAt: string;
+    createdBy: string;
+};
+
+export type Team = {
+    id: string;
+    name: string;
+    description: string;
+    members: {
+        userId: string;
+        role: 'admin' | 'member';
+    }[];
+};
+
+const API_URL = 'http://192.168.0.11:8000';
+
+class ApiService {
+    private static getHeaders(token: string) {
+        return {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+        };
+    }
+
+    static async getUserProjects(token: string): Promise<Project[]> {
+        const headers = this.getHeaders(token);
+        const response = await fetch(`${API_URL}/projects/user_projects`, {
+            method: 'GET',
+            headers,
+        });
+        const data = await response.json();
+        return data.projects;
+    }
+
+    static async createProject(token: string, project: Omit<Project, 'id' | 'createdAt'>): Promise<Project> {
+        const headers = this.getHeaders(token);
+        console.log(project);
+        const response = await fetch(`${API_URL}/projects/`, {
+            method: 'POST',
+            headers,
+            body: JSON.stringify(project),
+        });
+        return await response.json();
+    }
+}
+
+export default ApiService;
